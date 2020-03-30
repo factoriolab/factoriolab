a8209befd8c8117d4cd2b08308e2045ad8c52690
Doug Broad <dbroad@osisoft.com>
2020-03-30 11:39:19 -0400

    Update to Angular 9

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,14 +10,19 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to factorio!');
+    expect(page.getTitleText()).toEqual('factorio-lab app is running!');
   });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+    const logs = await browser
+      .manage()
+      .logs()
+      .get(logging.Type.BROWSER);
+    expect(logs).not.toContain(
+      jasmine.objectContaining({
+        level: logging.Level.SEVERE
+      } as logging.Entry)
+    );
   });
 });
--- src/app/models/recipe.ts
+++ src/app/models/recipe.ts
@@ -1,7 +1,7 @@
 export interface Recipe {
   id: string;
   time: number;
-  in: { [key: string]: number };
+  in?: { [key: string]: number };
   out?: { [key: string]: number };
   producers?: string[];
 }
