65594590a547da28170df4ae8c2a82ea7b88dff7
Doug Broad <dbroad@osisoft.com>
2020-05-27 16:15:16 -0400

    Hierarchy container tests

--- src/app/components/containers/hierarchy-container/hierarchy-container.component.spec.ts
+++ src/app/components/containers/hierarchy-container/hierarchy-container.component.spec.ts
@@ -35,4 +35,30 @@ describe('HierarchyContainerComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('steps', () => {
+    it('should handle no selection', () => {
+      component.selected = null;
+      expect(component.steps).toEqual([]);
+    });
+
+    it('should get steps from root node', () => {
+      const value = ['test'] as any;
+      component.selected = { id: 'root', children: value } as any;
+      expect(component.steps).toEqual(value);
+    });
+
+    it('should get steps from intermediate node', () => {
+      const value = ['test'] as any;
+      component.selected = { id: 'id', children: value } as any;
+      expect(component.steps.length).toEqual(2);
+      expect(component.steps[1]).toEqual(value[0]);
+    });
+
+    it('should get steps from leaf node', () => {
+      const value = { id: 'id' } as any;
+      component.selected = value;
+      expect(component.steps).toEqual([value]);
+    });
+  });
 });
