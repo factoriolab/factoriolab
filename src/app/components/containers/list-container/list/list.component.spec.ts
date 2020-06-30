869e35cbef3717adcfacdb577986b79f1a65017d
Doug Broad <dbroad@osisoft.com>
2020-06-29 23:44:02 -0400

    Imports cleanup

--- src/app/components/containers/list-container/list/list.component.spec.ts
+++ src/app/components/containers/list-container/list/list.component.spec.ts
@@ -4,17 +4,16 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { StoreModule } from '@ngrx/store';
 
 import * as Mocks from 'src/mocks';
+import { IconComponent, SelectComponent } from '~/components';
 import { Step, ItemId } from '~/models';
 import { RouterService } from '~/services/router.service';
 import { reducers, metaReducers } from '~/store';
 import { DatasetState } from '~/store/dataset';
+import { ItemsState } from '~/store/items';
 import { RecipesState } from '~/store/recipes';
 import { RecipeUtility } from '~/utilities';
 import { TestUtility } from '~/utilities/test';
-import { IconComponent } from '../../../icon/icon.component';
-import { SelectComponent } from '../../../select/select.component';
 import { ListComponent } from './list.component';
-import { ItemsState } from '~/store/items';
 
 @Component({
   selector: 'lab-test-list',
