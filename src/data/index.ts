0d0bd8f5ab1845a4ff92be9f1b7577bad6692d44
Doug Broad <dbroad@osisoft.com>
2020-07-23 10:58:18 -0400

    Add 0.17 support

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,10 +1,11 @@
 import { AppData } from '~/models';
 import app from './app/data.json';
+import base017 from './0.17/data.json';
 import base018 from './0.18/data.json';
 import research from './Research/data.json';
 
 export const data: AppData = {
   app,
-  base: [base018],
+  base: [base018, base017],
   mods: [research],
 };
