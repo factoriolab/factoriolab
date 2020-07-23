6837b67a0090df082f8b685016b804bd028f51e6
Doug Broad <dbroad@osisoft.com>
2020-07-23 11:51:05 -0400

    Add 0.16 data

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,11 +1,12 @@
 import { AppData } from '~/models';
 import app from './app/data.json';
+import base016 from './0.16/data.json';
 import base017 from './0.17/data.json';
 import base018 from './0.18/data.json';
 import research from './Research/data.json';
 
 export const data: AppData = {
   app,
-  base: [base018, base017],
+  base: [base018, base017, base016],
   mods: [research],
 };
