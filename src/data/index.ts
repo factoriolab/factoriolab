69434244a8074e81e56f232ec0ae9fc05b820fa9
Doug Broad <dbroad@osisoft.com>
2020-08-17 13:44:38 -0400

    Update data sets

--- src/data/index.ts
+++ src/data/index.ts
@@ -6,10 +6,10 @@ export const data: AppData = {
   app,
   base: [
     { id: '1.0', name: '1.0.0' },
-    { id: '0.18', name: '0.18.36' },
+    { id: '0.18', name: '0.18.47' },
     { id: '0.17', name: '0.17.79' },
     { id: '0.16', name: '0.16.51' },
-    { id: 'krastorio2', name: 'Krastorio 2' },
+    { id: 'krastorio2', name: 'Krastorio 2 (1.0.3)' },
   ],
   mods: [
     {
