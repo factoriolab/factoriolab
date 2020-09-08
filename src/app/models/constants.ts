1a01b555d270cc31cf6df010fa8db90b8255a110
Doug Broad <dbroad@osisoft.com>
2020-09-07 22:11:29 -0400

    Fix expanded factories, closes #175 (#176)

--- src/app/models/constants.ts
+++ src/app/models/constants.ts
@@ -5,6 +5,7 @@ export const WAGON_FLUID = new Rational(BigInt(25000));
 export const MODULE_ID = 'module';
 export const PIPE_ID = 'pipe';
 export const PRODUCTIVITY_LIMITATION = 'productivity-module';
+export const PUMPJACK_ID = 'pumpjack';
 export const ROCKET_PART_ID = 'rocket-part';
 export const SPACE_SCIENCE_ID = 'space-science-pack';
 export const WATER_ID = 'water';
