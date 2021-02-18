bbc2cf264b51ec04d269c34a044515fdae854a49
Doug Broad <dbroad@osisoft.com>
2021-02-18 11:45:41 -0500

    Fix Xander data set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,18 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { skip } from 'rxjs/operators';
+import { filter, skip, take, map } from 'rxjs/operators';
 
-import { Dataset, ItemId, Mod, Product, TITLE_DSP, TITLE_LAB } from './models';
+import { environment } from 'src/environments';
+import {
+  Dataset,
+  FuelType,
+  ItemId,
+  Mod,
+  Product,
+  TITLE_DSP,
+  TITLE_LAB,
+} from './models';
 import { ErrorService, RouterService } from './services';
 import { State } from './store';
 import { getProducts, getZipState } from './store/products';
@@ -84,6 +93,70 @@ export class AppComponent implements OnInit {
     });
     if (this.lsHidePoll) {
       this.showPoll = false;
+    }
+
+    // Used only in development to update hash files
+    // istanbul ignore next
+    if (!environment.production && !environment.testing) {
+      this.datasets$
+        .pipe(
+          filter((d) => !!d.length),
+          take(1),
+          map((d) => d[0].id)
+        )
+        .subscribe((id) => {
+          this.router.requestHash(id).subscribe((h) => {
+            this.data$.subscribe((d) => {
+              console.log(id);
+              console.log(
+                JSON.stringify(
+                  d.complexRecipeIds.filter((i) => !d.itemEntities[i])
+                )
+              );
+              for (const id of d.itemIds
+                .sort()
+                .filter((i) => h.items.indexOf(i) === -1)) {
+                h.items.push(id);
+              }
+              for (const id of d.beaconIds
+                .sort()
+                .filter((i) => h.beacons.indexOf(i) === -1)) {
+                h.beacons.push(id);
+              }
+              for (const id of d.beltIds
+                .sort()
+                .filter((i) => h.belts.indexOf(i) === -1)) {
+                h.belts.push(id);
+              }
+              for (const id of d.fuelIds[FuelType.Chemical]
+                .sort()
+                .filter((i) => h.fuels.indexOf(i) === -1)) {
+                h.fuels.push(id);
+              }
+              for (const id of [...d.cargoWagonIds, ...d.fluidWagonIds]
+                .sort()
+                .filter((i) => h.wagons.indexOf(i) === -1)) {
+                h.wagons.push(id);
+              }
+              for (const id of d.factoryIds
+                .sort()
+                .filter((i) => h.factories.indexOf(i) === -1)) {
+                h.factories.push(id);
+              }
+              for (const id of d.moduleIds
+                .sort()
+                .filter((i) => h.modules.indexOf(i) === -1)) {
+                h.modules.push(id);
+              }
+              for (const id of d.recipeIds
+                .sort()
+                .filter((i) => h.recipes.indexOf(i) === -1)) {
+                h.recipes.push(id);
+              }
+              console.log(JSON.stringify(h));
+            });
+          });
+        });
     }
   }
 
