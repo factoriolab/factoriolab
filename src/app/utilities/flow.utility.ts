a2bc541b0095c961bee000ce1260c96478a6a578
Doug Broad <dbroad@osisoft.com>
2021-04-09 13:52:08 -0400

    Scale fluid links by 1/10, closes #447

--- src/app/utilities/flow.utility.ts
+++ src/app/utilities/flow.utility.ts
@@ -38,17 +38,23 @@ export class FlowUtility {
 
         if (step.itemId === step.recipeId && step.parents) {
           for (const i of Object.keys(step.parents)) {
+            const item = data.itemEntities[step.itemId];
             sankey.links.push({
               target: i,
               source: step.recipeId,
-              value: this.linkValue(value, step.parents[i], linkValue),
+              value: this.linkValue(
+                value,
+                step.parents[i],
+                linkValue,
+                item.stack
+              ),
               dispValue: this.linkDisp(
                 value,
                 step.parents[i],
                 linkValue,
                 linkPrecision
               ),
-              name: data.itemEntities[step.itemId].name,
+              name: item.name,
               color: icon.color,
             });
           }
@@ -62,17 +68,18 @@ export class FlowUtility {
           const outStep = steps.find((s) => s.itemId === outId);
           const outValue = this.stepLinkValue(outStep, linkValue);
           const percent = step.outputs[outId];
+          const item = data.itemEntities[outId];
           sankey.links.push({
             target: outId,
             source: step.recipeId,
-            value: this.linkValue(outValue, percent, linkValue),
+            value: this.linkValue(outValue, percent, linkValue, item.stack),
             dispValue: this.linkDisp(
               outValue,
               percent,
               linkValue,
               linkPrecision
             ),
-            name: data.itemEntities[outId].name,
+            name: item.name,
             color: data.iconEntities[outId].color,
           });
         }
@@ -98,7 +105,12 @@ export class FlowUtility {
               sankey.links.push({
                 target: i,
                 source: step.itemId,
-                value: this.linkValue(value, step.parents[i], linkValue),
+                value: this.linkValue(
+                  value,
+                  step.parents[i],
+                  linkValue,
+                  item.stack
+                ),
                 dispValue: this.linkDisp(
                   value,
                   step.parents[i],
@@ -142,7 +154,8 @@ export class FlowUtility {
   static linkValue(
     value: Rational,
     percent: Rational,
-    linkValue: LinkValue
+    linkValue: LinkValue,
+    stack: number
   ): number {
     if (linkValue === LinkValue.None) {
       return 1;
@@ -150,6 +163,11 @@ export class FlowUtility {
 
     if (linkValue === LinkValue.Percent) {
       return percent.toNumber() || MIN_LINK_VALUE;
+    }
+
+    // Scale link size for fluids to 1/10
+    if (linkValue === LinkValue.Items && !stack) {
+      value = value.div(Rational.from(10));
     }
 
     return value.mul(percent).toNumber() || MIN_LINK_VALUE;
