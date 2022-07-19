7d2830f9cff099c8e2b5d0bad958d5633aeb53e7
Doug Broad <dcbroad3@gmail.com>
2022-07-18 23:07:24 -0400

    Don't break coi urls

--- src/data/index.ts
+++ src/data/index.ts
@@ -78,12 +78,12 @@ export const data: AppData = {
     'ang',
     'msc',
     'sfy',
+    'coi',
     'qat',
     '1.1e',
     '248',
     '248o',
     '248kr2sxp',
     'aai',
-    'coi',
   ],
 };
