10045aab66c74ac5c3867a84b11fec5b361c7dc5
Doug Broad <dcbroad3@gmail.com>
2022-08-16 13:52:03 -0400

    Add small icon pipe and styles

--- src/app/shared/pipes/icon-class.pipe.ts
+++ src/app/shared/pipes/icon-class.pipe.ts
@@ -8,3 +8,12 @@ export class IconClassPipe implements PipeTransform {
     return `lab-icon ${value}`;
   }
 }
+
+@Pipe({ name: 'iconSmClass' })
+export class IconSmClassPipe implements PipeTransform {
+  transform(value: string | null | undefined): string {
+    if (value == null) return '';
+
+    return `lab-icon-sm ${value}`;
+  }
+}
