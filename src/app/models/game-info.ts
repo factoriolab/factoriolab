40d0f183b1c64ed5ceaf1c8016676051776979fd
Doug Broad <dcbroad3@gmail.com>
2022-08-22 11:15:31 -0400

    Try webp

--- src/app/models/game-info.ts
+++ src/app/models/game-info.ts
@@ -10,35 +10,35 @@ interface GameInfo {
 
 export const gameInfo: Record<Game, GameInfo> = {
   [Game.None]: {
-    icon: 'assets/factorio.png',
+    icon: 'assets/factorio.webp',
     route: 'factorio',
     title: 'title.lab',
     meta: 'FactorioLab',
     modId: '1.1',
   },
   [Game.Factorio]: {
-    icon: 'assets/factorio.png',
+    icon: 'assets/factorio.webp',
     route: 'factorio',
     title: 'title.lab',
     meta: 'Factorio',
     modId: '1.1',
   },
   [Game.CaptainOfIndustry]: {
-    icon: 'assets/coi.png',
+    icon: 'assets/coi.webp',
     route: 'coi',
     title: 'title.coi',
     meta: 'Captain of Industry',
     modId: 'coi',
   },
   [Game.DysonSphereProgram]: {
-    icon: 'assets/dsp.png',
+    icon: 'assets/dsp.webp',
     route: 'dsp',
     title: 'title.dsp',
     meta: 'Dyson Sphere Program',
     modId: 'dsp',
   },
   [Game.Satisfactory]: {
-    icon: 'assets/satisfactory.png',
+    icon: 'assets/satisfactory.webp',
     route: 'satisfactory',
     title: 'title.sfy',
     meta: 'Satisfactory',
