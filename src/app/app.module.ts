e55f7ff39344b1ae6d02062815fc43fdf676bf79
Doug Broad <dcbroad3@gmail.com>
2022-09-08 09:30:45 -0400

    Implement PWA, closes #384 (#836)
    * Increment version
    
    * Update colors
    
    * Update tests
    
    * Minor tweaks
    
    * Update angular
    
    * Update other deps
    
    * PWA implementation and icons
    
    * Increment version
    
    * Undo
    
    * Fix flow color, total row sizing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,6 +31,7 @@ import { AnalyticsEffects } from './store/analytics.effects';
 import { DatasetsEffects } from './store/datasets/datasets.effects';
 import { FactoriesEffects } from './store/factories/factories.effects';
 import { ProductsEffects } from './store/products/products.effects';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 function initializeApp(
   primengConfig: PrimeNGConfig,
@@ -91,6 +92,12 @@ function initializeApp(
     /** App modules */
     AppRoutingModule,
     AppSharedModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [
     { provide: APP_BASE_HREF, useValue: environment.baseHref },
--- src/app/models/flow.ts
+++ src/app/models/flow.ts
@@ -21,7 +21,7 @@ export interface FlowStyle {
 const darkTheme: FlowStyle = {
   node: {
     [NodeType.Input]: { color: '#044868', background: '#B3E5FC' },
-    [NodeType.Recipe]: { color: '#212529', background: '#FF9A4D' },
+    [NodeType.Recipe]: { color: '#212529', background: '#ffbe4d' },
     [NodeType.Surplus]: { color: '#73000c', background: '#FFCDD2' },
     [NodeType.Output]: { color: '#224a23', background: '#C8E6C9' },
   },
