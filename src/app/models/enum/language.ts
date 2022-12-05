872311c0fec728b284f76af289c1c7b5716c2f4d
JingBh <jingbohao@yeah.net>
2022-12-05 16:03:40 +0800

    Update Chinese translations for the new version

--- src/app/models/enum/language.ts
+++ src/app/models/enum/language.ts
@@ -9,5 +9,5 @@ export enum Language {
 export const languageOptions: SelectItem<Language>[] = [
   { label: 'English', value: Language.English },
   { label: 'Française', value: Language.French },
-  { label: '简体中文 - Outdated', value: Language.Chinese },
+  { label: '简体中文', value: Language.Chinese },
 ];
