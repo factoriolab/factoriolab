3b5d1bd00cc42da5836585c6743dba3335b07722
Doug Broad <dcbroad3@gmail.com>
2023-01-03 16:10:27 -0500

    Invert light icons, closes #922 (#933)
    * Invert light icons
    
    * Increment version
    
    * Update tests

--- src/app/models/data/icon.ts
+++ src/app/models/data/icon.ts
@@ -2,4 +2,6 @@ export interface Icon {
   id: string;
   position: string;
   file?: string;
+  /** If true, icon is mostly white, and should be inverted in light mode */
+  invertLight?: boolean;
 }
