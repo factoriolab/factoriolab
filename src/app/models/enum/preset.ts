29b5a7019bd9fbb0258a69bd451042d9b346589c
Doug Broad <dcbroad3@gmail.com>
2023-01-30 13:58:50 -0500

    Fix packaged turbofuel stack size, closes #961, closes #964 (#967)
    * Increment version
    
    * Fix data
    
    * Fix presets for Satisfactory

--- src/app/models/enum/preset.ts
+++ src/app/models/enum/preset.ts
@@ -17,14 +17,14 @@ export function presetOptions(game: Game): SelectItem<Preset>[] {
         { value: Preset.Beacon8, label: 'options.preset.beacon8' },
         { value: Preset.Beacon12, label: 'options.preset.beacon12' },
       ]
-    : game === Game.CaptainOfIndustry
+    : game === Game.DysonSphereProgram
     ? [
         { value: Preset.Minimum, label: 'options.preset.minimum' },
         { value: Preset.Modules, label: 'options.preset.upgraded' },
+        { value: Preset.Beacon8, label: 'options.preset.proliferated' },
       ]
     : [
         { value: Preset.Minimum, label: 'options.preset.minimum' },
         { value: Preset.Modules, label: 'options.preset.upgraded' },
-        { value: Preset.Beacon8, label: 'options.preset.proliferated' },
       ];
 }
