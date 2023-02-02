e42b15c84a1378d343a99661bd9ddc70c93b60f1
Doug Broad <dcbroad3@gmail.com>
2023-02-02 14:36:17 -0500

    Picker fixes, closes #975, closes #978 (#984)
    * Picker fixes
    
    * Increment version
    
    * Add tests
    
    * Increment version

---
+++ src/app/directives/tabview-override.directive.spec.ts
@@ -0,0 +1,41 @@
+import { Component, ViewChild } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DomHandler } from 'primeng/dom';
+import { TabView } from 'primeng/tabview';
+
+import { TestModule } from 'src/tests';
+
+@Component({
+  template: `<p-tabView></p-tabView>`,
+})
+class TestTabViewOverrideDirectiveComponent {
+  @ViewChild(TabView) child?: TabView;
+}
+
+describe('TabViewOverrideDirective', () => {
+  let component: TestTabViewOverrideDirectiveComponent;
+  let fixture: ComponentFixture<TestTabViewOverrideDirectiveComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TestTabViewOverrideDirectiveComponent],
+      imports: [TestModule],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestTabViewOverrideDirectiveComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('updateButtonState', () => {
+    it('should disable forward button if scrollLeft is greater than scrollWidth - width', () => {
+      spyOn(DomHandler, 'getWidth').and.returnValue(10000);
+      component.child?.updateButtonState();
+      expect(component.child?.forwardIsDisabled).toBeTrue();
+    });
+  });
+});
---
+++ src/app/directives/tabview-override.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, Self } from '@angular/core';
+import { DomHandler } from 'primeng/dom';
+import { TabView } from 'primeng/tabview';
+
+// eslint-disable-next-line @angular-eslint/directive-selector
+@Directive({ selector: 'p-tabView' })
+export class TabViewOverrideDirective {
+  constructor(@Self() tabView: TabView) {
+    // Override PrimeNG function to use >= in place of === when checking
+    // whether to show forward button
+    tabView.updateButtonState = (): void => {
+      const content = tabView.content.nativeElement as HTMLElement;
+      const { scrollLeft, scrollWidth } = content;
+      const width = DomHandler.getWidth(content);
+
+      tabView.backwardIsDisabled = scrollLeft === 0;
+      tabView.forwardIsDisabled = scrollLeft >= scrollWidth - width;
+    };
+  }
+}
