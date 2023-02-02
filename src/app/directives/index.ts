897a83fe9ca06fa49a9fed1f4288e2707c93da79
Doug Broad <dcbroad3@gmail.com>
2023-02-02 14:36:17 -0500

    Picker fixes, closes #975, closes #978 (#984)
    * Picker fixes
    
    * Increment version
    
    * Add tests
    
    * Increment version

--- src/app/directives/index.ts
+++ src/app/directives/index.ts
@@ -1,9 +1,11 @@
 import { DropdownBaseDirective } from './dropdown-base.directive';
 import { DropdownTranslateDirective } from './dropdown-translate.directive';
+import { TabViewOverrideDirective } from './tabview-override.directive';
 import { ValidateNumberDirective } from './validate-number.directive';
 
 export const directives = [
   DropdownBaseDirective,
   DropdownTranslateDirective,
+  TabViewOverrideDirective,
   ValidateNumberDirective,
 ];
