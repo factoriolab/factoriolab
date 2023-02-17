5ad02222b31ca04fd937baf04808b9dd44e3b716
Doug Broad <dcbroad3@gmail.com>
2023-02-17 17:02:42 -0500

    Update theme.service.spec.ts

--- src/app/services/theme.service.spec.ts
+++ src/app/services/theme.service.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 import { MockStore } from '@ngrx/store/testing';
-import { first } from 'rxjs';
 
 import { CategoryId, ItemId, Mocks, TestModule } from 'src/tests';
 import { Theme } from '~/models';
