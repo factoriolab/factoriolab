43cd0498513cc1f2ba9c0f723eb93976c893b8df
Doug Broad <dcbroad3@gmail.com>
2023-02-21 10:29:37 -0500

    Black theme, closes #992 (#1005)
    * Increment version
    
    * Black theme
    
    * Minor tweaks
    
    * Update theme.service.spec.ts
    
    * Update theme.service.spec.ts
    
    * Update tests

--- src/app/models/enum/theme.ts
+++ src/app/models/enum/theme.ts
@@ -1,11 +1,13 @@
 import { SelectItem } from 'primeng/api';
 
 export enum Theme {
-  Dark,
-  Light,
+  Dark = 'dark',
+  Light = 'light',
+  Black = 'black',
 }
 
 export const themeOptions: SelectItem<Theme>[] = [
   { label: 'options.theme.dark', value: Theme.Dark },
   { label: 'options.theme.light', value: Theme.Light },
+  { label: 'options.theme.black', value: Theme.Black },
 ];
