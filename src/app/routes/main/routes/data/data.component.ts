7f2fc81df7e7ec47cccf973d141a3b4898330c18
Doug Broad <dcbroad3@gmail.com>
2023-04-17 14:49:52 -0400

    Partial work on data landing page

--- src/app/routes/main/routes/data/data.component.ts
+++ src/app/routes/main/routes/data/data.component.ts
@@ -1,11 +1,18 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { combineLatest, map } from 'rxjs';
+
+import { LabState, Recipes } from '~/store';
 
 @Component({
-  selector: 'lab-data',
   templateUrl: './data.component.html',
   styleUrls: ['./data.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DataComponent {
+  vm$ = combineLatest([this.store.select(Recipes.getAdjustedDataset)]).pipe(
+    map(([data]) => ({ data }))
+  );
 
+  constructor(private store: Store<LabState>) {}
 }
--- src/app/routes/main/routes/data/data.routes.ts
+++ src/app/routes/main/routes/data/data.routes.ts
@@ -4,29 +4,25 @@ import { DataComponent } from './data.component';
 
 export const routes: Routes = [
   {
+    path: 'category/:id',
+    loadComponent: () =>
+      import('./routes/category/category.component').then(
+        (c) => c.CategoryComponent
+      ),
+  },
+  {
+    path: 'item/:id',
+    loadComponent: () =>
+      import('./routes/item/item.component').then((c) => c.ItemComponent),
+  },
+  {
+    path: 'recipe/:id',
+    loadComponent: () =>
+      import('./routes/recipe/recipe.component').then((c) => c.RecipeComponent),
+  },
+  {
     path: '',
     component: DataComponent,
-    children: [
-      {
-        path: 'category/:id',
-        loadComponent: () =>
-          import('./routes/category/category.component').then(
-            (c) => c.CategoryComponent
-          ),
-      },
-      {
-        path: 'item/:id',
-        loadComponent: () =>
-          import('./routes/item/item.component').then((c) => c.ItemComponent),
-      },
-      {
-        path: 'recipe/:id',
-        loadComponent: () =>
-          import('./routes/recipe/recipe.component').then(
-            (c) => c.RecipeComponent
-          ),
-      },
-    ],
   },
 ];
 
