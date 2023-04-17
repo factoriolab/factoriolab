c5dfc1bab65154e674cfc426968c9e5ce0b31ecc
Doug Broad <dcbroad3@gmail.com>
2023-04-17 12:56:14 -0400

    Change fallback for step id pipe

--- src/app/pipes/step-id.pipe.ts
+++ src/app/pipes/step-id.pipe.ts
@@ -5,7 +5,9 @@ import { Step } from '~/models';
 @Pipe({ name: 'stepId' })
 export class StepIdPipe implements PipeTransform {
   static transform(value: Step): string {
-    return value.itemId ?? value.recipeObjectiveId ?? value.recipeId ?? '';
+    return (
+      value.itemId ?? value.recipeObjectiveId ?? value.recipeId ?? value.id
+    );
   }
 
   transform(value: Step): string {
