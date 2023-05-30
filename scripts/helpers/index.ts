b95dd112f04750c76fe3f26cd52bae8b23ef5a3a
Doug Broad <dcbroad3@gmail.com>
2023-05-30 12:21:12 -0400

    Update scripts

--- scripts/helpers/index.ts
+++ scripts/helpers/index.ts
@@ -8,3 +8,14 @@ export function getJsonData<T>(file: string, sanitize = false): T {
 
   return JSON.parse(str) as T;
 }
+
+export function coerceArray<T>(
+  value: T[] | Record<string, T> | null | undefined
+): T[] {
+  if (value == null) return [];
+
+  if (Array.isArray(value)) return value;
+
+  const record = value;
+  return Object.keys(record).map((k) => record[k]);
+}
