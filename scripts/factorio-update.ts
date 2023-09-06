8fd65d872f4976fe15d5a0b8608808f8bbf63c86
Doug Broad <dcbroad3@gmail.com>
2023-09-05 23:12:48 -0400

    Add logs to update script

--- scripts/factorio-update.ts
+++ scripts/factorio-update.ts
@@ -1,4 +1,5 @@
 import { exec, spawn } from 'child_process';
+import fs from 'fs';
 
 /**
  * This script is intended to update a specific Factorio mod set or all sets
@@ -60,34 +61,41 @@ function logTime(msg: string): void {
   console.log(formatTime(allTime), formatTime(stepTime), msg);
 }
 
-async function runTsNodeCmd(cmd: string, mod: string): Promise<void> {
+const logsPath = `./scripts/logs`;
+const logPath = `${logsPath}/factorio-update.log`;
+if (!fs.existsSync(logsPath)) fs.mkdirSync(logsPath);
+if (fs.existsSync(logPath)) fs.rmSync(logPath);
+/** Log command output to separate log file to minimize console output */
+function logDebug(msg: string): void {
+  fs.appendFileSync(logPath, `${msg}`);
+}
+
+async function runCommand(
+  command: string,
+  args?: string[],
+): Promise<number | null> {
   return new Promise((resolve, reject) => {
-    exec(`npm run ${cmd} ${mod}`, (err) => {
-      if (err) reject(err);
-      resolve();
-    });
+    const proc = spawn(command, args);
+    proc.stdout.on('data', (data) => logDebug(data));
+    proc.stderr.on('data', (data) => logDebug(data));
+    proc.on('error', (code) => reject(code));
+    proc.on('exit', (code) => resolve(code));
   });
 }
 
+async function runNpmCommand(cmd: string, mod: string): Promise<number | null> {
+  return runCommand('npm.cmd', ['run', cmd, mod]);
+}
+
 /** Prep for a dump of Factorio using the factorio-prep.ts script */
-async function runFactorioPrep(mod: string): Promise<void> {
-  return runTsNodeCmd('factorio-prep', mod);
+async function runFactorioPrep(mod: string): Promise<number | null> {
+  return runNpmCommand('factorio-prep', mod);
 }
 
 /** Start a dump of Factorio using the factorio-dump.bat script */
 async function runFactorioDump(): Promise<number | null> {
-  return new Promise((resolve, reject) => {
-    const bat = require.resolve('./factorio-dump.bat');
-    const ls = spawn(bat);
-
-    ls.on('error', (code) => {
-      reject(code);
-    });
-
-    ls.on('exit', (code) => {
-      resolve(code);
-    });
-  });
+  const bat = require.resolve('./factorio-dump.bat');
+  return runCommand(bat);
 }
 
 /** Check whether Factorio is running after a delay */
@@ -117,10 +125,11 @@ async function waitForFactorioToExit(waitMs = 30000): Promise<void> {
 }
 
 /** Build a Factorio mod data set using the factorio-build.ts script */
-async function runFactorioBuild(mod: string): Promise<void> {
-  return runTsNodeCmd('factorio-build', mod);
+async function runFactorioBuild(mod: string): Promise<number | null> {
+  return runNpmCommand('factorio-build', mod);
 }
 
+/** Run all scripts required to update a Factorio mod set */
 async function updateMod(mod: string): Promise<void> {
   await runFactorioPrep(mod);
   await runFactorioDump();
@@ -128,6 +137,7 @@ async function updateMod(mod: string): Promise<void> {
   await runFactorioBuild(mod);
 }
 
+/** Run all scripts required to update an array of Factorio mod sets */
 async function updateMods(mods: string[]): Promise<void> {
   for (let i = 0; i < mods.length; i++) {
     const mod = mods[i];
