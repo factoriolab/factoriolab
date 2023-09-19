77741769ce63eb2c4eb5e7c6725de99a854afba5
Doug Broad <dcbroad3@gmail.com>
2023-09-19 10:05:05 -0400

    Update data

--- scripts/factorio-build.ts
+++ scripts/factorio-build.ts
@@ -625,13 +625,14 @@ async function processMod(): Promise<void> {
       techId[techRaw.name] = techRaw.name;
     }
 
-    if (!techData.hidden && techData.enabled !== false)
+    if (!techData.hidden && techData.enabled !== false) {
       technologySet.add(techRaw);
 
-    for (const effect of coerceArray(techData.effects)) {
-      if (M.isUnlockRecipeModifier(effect)) {
-        recipesUnlocked[effect.recipe] = techId[techRaw.name];
-        technologySet.add(techRaw);
+      for (const effect of coerceArray(techData.effects)) {
+        if (M.isUnlockRecipeModifier(effect)) {
+          recipesUnlocked[effect.recipe] = techId[techRaw.name];
+          technologySet.add(techRaw);
+        }
       }
     }
   }
