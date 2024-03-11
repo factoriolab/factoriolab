b4a11ffe1ccfdc7936190e066aa8961601756f48
Doug Broad <dcbroad3@gmail.com>
2024-03-11 13:30:57 -0400

    Update PrimeNG, fix tabmenu

--- src/app/routes/main/main.component.ts
+++ src/app/routes/main/main.component.ts
@@ -7,7 +7,9 @@ import {
 } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { TranslateService } from '@ngx-translate/core';
 import { MenuItem } from 'primeng/api';
+import { map } from 'rxjs';
 
 import { SimplexResultType } from '~/models';
 import { ContentService, ErrorService } from '~/services';
@@ -26,29 +28,36 @@ export class MainComponent {
   router = inject(Router);
   store = inject(Store<LabState>);
   errorSvc = inject(ErrorService);
+  translateSvc = inject(TranslateService);
 
   gameInfo = this.store.selectSignal(Settings.getGameInfo);
   mod = this.store.selectSignal(Settings.getMod);
   result = this.store.selectSignal(Objectives.getMatrixResult);
 
   isResetting = false;
-  tabItems: MenuItem[] = [
-    {
-      label: 'app.list',
-      icon: 'fa-solid fa-list',
-      routerLink: 'list',
-    },
-    {
-      label: 'app.flow',
-      icon: 'fa-solid fa-diagram-project',
-      routerLink: 'flow',
-    },
-    {
-      label: 'app.data',
-      icon: 'fa-solid fa-database',
-      routerLink: 'data',
-    },
-  ];
+
+  tabItems$ = this.contentSvc.lang$.pipe(
+    map((): MenuItem[] => [
+      {
+        label: this.translateSvc.instant('app.list'),
+        icon: 'fa-solid fa-list',
+        routerLink: 'list',
+        queryParamsHandling: 'preserve',
+      },
+      {
+        label: this.translateSvc.instant('app.flow'),
+        icon: 'fa-solid fa-diagram-project',
+        routerLink: 'flow',
+        queryParamsHandling: 'preserve',
+      },
+      {
+        label: this.translateSvc.instant('app.data'),
+        icon: 'fa-solid fa-database',
+        routerLink: 'data',
+        queryParamsHandling: 'preserve',
+      },
+    ]),
+  );
 
   SimplexResultType = SimplexResultType;
 
