bf39363ac2e297b8deeedf2443e6d90b86433729
Doug Broad <dcbroad3@gmail.com>
2024-04-09 12:53:54 -0400

    fix: retain text as entered in number fields, closes #1394 (#1398)

--- src/app/directives/validate-number.directive.ts
+++ src/app/directives/validate-number.directive.ts
@@ -23,17 +23,15 @@ export class ValidateNumberDirective implements Validator {
   maximum = input<Rational | null>(null);
 
   validate(
-    control: AbstractControl<Rational | null | undefined>,
+    control: AbstractControl<string | null | undefined>,
   ): ValidationErrors | null {
     if (control.value == null) return null;
 
     try {
+      const value = rational(control.value);
       const min = this.minimum();
       const max = this.maximum();
-      if (
-        (min == null || control.value.gte(min)) &&
-        (max == null || control.value.lte(max))
-      )
+      if ((min == null || value.gte(min)) && (max == null || value.lte(max)))
         return null;
     } catch {
       // ignore error
--- src/app/vendor.module.ts
+++ src/app/vendor.module.ts
@@ -10,6 +10,7 @@ import { CheckboxModule } from 'primeng/checkbox';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { DialogModule } from 'primeng/dialog';
 import { DividerModule } from 'primeng/divider';
+import { ConnectedOverlayScrollHandler, DomHandler } from 'primeng/dom';
 import { DropdownModule } from 'primeng/dropdown';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { InputTextModule } from 'primeng/inputtext';
@@ -17,7 +18,7 @@ import { InputTextareaModule } from 'primeng/inputtextarea';
 import { MenuModule } from 'primeng/menu';
 import { MessagesModule } from 'primeng/messages';
 import { MultiSelectModule } from 'primeng/multiselect';
-import { OrderListModule } from 'primeng/orderlist';
+import { OrderList, OrderListModule } from 'primeng/orderlist';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { RippleModule } from 'primeng/ripple';
 import { ScrollPanelModule } from 'primeng/scrollpanel';
@@ -28,6 +29,24 @@ import { TabViewModule } from 'primeng/tabview';
 import { ToastModule } from 'primeng/toast';
 import { ToggleButtonModule } from 'primeng/togglebutton';
 import { TooltipModule } from 'primeng/tooltip';
+
+/**
+ * Workaround for https://github.com/primefaces/primeng/issues/12114.
+ * Manually add the main window to the list of scrollable parents, so that when
+ * the main window is scrolled, dropdowns will be closed.
+ */
+ConnectedOverlayScrollHandler.prototype.bindScrollListener = function (
+  this,
+): void {
+  this.scrollableParents = DomHandler.getScrollableParents(this.element);
+  this.scrollableParents.push(window);
+  for (const parent of this.scrollableParents)
+    parent.addEventListener('scroll', this.listener);
+};
+
+// istanbul ignore next
+/** Allow entering spaces inside orderlist items */
+OrderList.prototype.onSpaceKey = function (): void {};
 
 const modules = [
   // primeng
