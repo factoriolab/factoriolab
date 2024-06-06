352fc5384fd2bfca85f6b12c95a564c42268642d
emeiziying <31397353+emeiziying@users.noreply.github.com>
2024-06-06 21:50:00 +0800

    build: Add Mac script support and fit latest version of factorio's prototype-api (#1409)
    1. now you can run factorio-dump:mac to get factorio's data raw;
    2. fit a new built-in type DataExtendMethod for latest 1.1.107
    
    I didn't commit latest factorio.models.ts, maybe you can run and build a
    new version of this application.

---
+++ scripts/factorio-dump.sh
@@ -0,0 +1,5 @@
+cd ~/Library/Application\ Support/Steam/steamapps/common/Factorio/factorio.app/Contents/MacOS
+
+./factorio --dump-data
+./factorio --dump-prototype-locale
+/Applications/Steam.app/Contents/MacOS/steam_osx -applaunch 427520 --dump-icon-sprites
