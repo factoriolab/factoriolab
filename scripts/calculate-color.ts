bbdc0543494482e650694d92ab3db466203c8b3f
Doug Broad <dcbroad3@gmail.com>
2024-10-18 09:49:37 -0400

    Revert calculate-color temp script

--- scripts/calculate-color.ts
+++ scripts/calculate-color.ts
@@ -12,42 +12,30 @@ let mods = process.argv.slice(2);
 if (mods.length === 0) mods = data.mods.map((m) => m.id);
 
 /** Run all scripts required to update an array of Factorio mod sets */
-function updateMods(mods: string[]): void {
+async function updateMods(mods: string[]): Promise<void> {
   for (let i = 0; i < mods.length; i++) {
     const mod = mods[i];
     const modPath = `./src/data/${mod}`;
     const modDataPath = `${modPath}/data.json`;
     const modData = getJsonData(modDataPath) as ModData;
-    // const image = fs.readFileSync(`${modPath}/icons.webp`);
+    const image = fs.readFileSync(`${modPath}/icons.webp`);
 
-    modData.groups = (modData as any).categories;
-    delete (modData as any).categories;
-
-    modData.items.forEach((i) => {
-      i.group = (i as any).category;
-      delete (i as any).category;
-    });
-    modData.recipes.forEach((i) => {
-      i.group = (i as any).category;
-      delete (i as any).category;
-    });
-
-    // modData.icons = await Promise.all(
-    //   modData.icons.map(async (icon) => {
-    //     const coords = /-?(\d+)px -?(\d+)px/.exec(icon.position);
-    //     const left = Number(coords?.[1]);
-    //     const top = Number(coords?.[2]);
-    //     const color = await getAverageColor(image, {
-    //       mode: 'precision',
-    //       top,
-    //       left,
-    //       width: 64,
-    //       height: 64,
-    //     });
-    //     icon.color = color.hex;
-    //     return icon;
-    //   }),
-    // );
+    modData.icons = await Promise.all(
+      modData.icons.map(async (icon) => {
+        const coords = /-?(\d+)px -?(\d+)px/.exec(icon.position);
+        const left = Number(coords?.[1]);
+        const top = Number(coords?.[2]);
+        const color = await getAverageColor(image, {
+          mode: 'precision',
+          top,
+          left,
+          width: 64,
+          height: 64,
+        });
+        icon.color = color.hex;
+        return icon;
+      }),
+    );
     fs.writeFileSync(modDataPath, JSON.stringify(modData));
 
     logTime(
@@ -60,4 +48,4 @@ logTime(
   `Starting update for ${mods.length.toString()} mod${mods.length > 1 ? 's' : ''}...`,
 );
 
-updateMods(mods);
+void updateMods(mods);
