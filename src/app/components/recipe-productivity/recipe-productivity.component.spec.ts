88592b13600eef1c3d5aa605d8b5957ddb2b7ba0
Doug Broad <dcbroad3@gmail.com>
2024-10-31 11:15:46 -0400

    Update tests

--- src/app/components/recipe-productivity/recipe-productivity.component.spec.ts
+++ src/app/components/recipe-productivity/recipe-productivity.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { rational } from '~/models/rational';
+import { Mocks, RecipeId, TestModule } from '~/tests';
+
 import { RecipeProductivityComponent } from './recipe-productivity.component';
 
 describe('RecipeProductivityComponent', () => {
@@ -8,9 +11,8 @@ describe('RecipeProductivityComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RecipeProductivityComponent]
-    })
-    .compileComponents();
+      imports: [TestModule, RecipeProductivityComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(RecipeProductivityComponent);
     component = fixture.componentInstance;
@@ -19,5 +21,58 @@ describe('RecipeProductivityComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('modified', () => {
+    it('should determine whether the value matches the initial state', () => {
+      component.reset();
+      expect(component.modified).toBeFalse();
+      component.editValue[RecipeId.SteelPlate] = rational(10n);
+      expect(component.modified).toBeTrue();
+    });
+  });
+
+  describe('open', () => {
+    it('should set up the editValue and show the dialog', () => {
+      const data = Mocks.getDataset();
+      data.canProdUpgradeRecipeIds.push(RecipeId.SteelPlate);
+      spyOn(component, 'data').and.returnValue(data);
+      spyOn(component, 'show');
+      component.editValue = null as any;
+      component.open();
+      expect(component.editValue).toEqual({
+        [RecipeId.SteelPlate]: rational.zero,
+      });
+      expect(component.show).toHaveBeenCalled();
+    });
+  });
+
+  describe('reset', () => {
+    it('should set the value back to the initial state', () => {
+      const data = Mocks.getDataset();
+      data.canProdUpgradeRecipeIds.push(RecipeId.SteelPlate);
+      spyOn(component, 'data').and.returnValue(data);
+      component.editValue = null as any;
+      component.reset();
+      expect(component.editValue).toEqual({
+        [RecipeId.SteelPlate]: rational.zero,
+      });
+    });
+  });
+
+  describe('save', () => {
+    it('should dispatch the action', () => {
+      const data = Mocks.getDataset();
+      data.canProdUpgradeRecipeIds.push(RecipeId.SteelPlate);
+      spyOn(component, 'data').and.returnValue(data);
+      spyOn(component.recipesSvc, 'updateEntityField');
+      component.save();
+      expect(component.recipesSvc.updateEntityField).toHaveBeenCalledWith(
+        RecipeId.SteelPlate,
+        'productivity',
+        undefined,
+        rational.zero,
+      );
+    });
   });
 });
