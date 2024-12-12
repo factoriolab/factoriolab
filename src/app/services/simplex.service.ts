22f4664e8dda93bf7e2782806997fe1cc631d4cf
Doug Broad <dcbroad3@gmail.com>
2024-12-12 16:21:18 -0500

    Fix step recipe assignment

--- src/app/services/simplex.service.ts
+++ src/app/services/simplex.service.ts
@@ -916,24 +916,28 @@ export class SimplexService {
     }
 
     // Find best recipe match for remaining steps
-    const potentials: Entities<string[]> = {};
+    const outputRecipes: Entities<string[]> = {};
+    const allRecipes: Entities<string[]> = {};
     for (const step of steps.filter((s) => s.recipeId == null)) {
       if (step.itemId) {
         const itemId = step.itemId;
-        const potentialRecipes = recipes.filter(
-          (r) => r.output[itemId] != null,
+        const potentialRecipes = recipes.filter((r) => r.output[itemId]);
+        const order = potentialRecipes.sort((a, b) =>
+          b.output[itemId].sub(a.output[itemId]).toNumber(),
         );
-        potentials[itemId] = potentialRecipes
-          .sort((a, b) => b.output[itemId].sub(a.output[itemId]).toNumber())
+        outputRecipes[itemId] = order
+          .filter((r) => r.output[itemId].gt(rational.zero))
           .map((r) => r.id);
+        allRecipes[itemId] = order.map((r) => r.id);
       }
     }
 
-    const order = Object.keys(potentials).sort(
-      (a, b) => potentials[a].length - potentials[b].length,
+    // Sort only based on output recipes, ignore input recipes
+    const order = Object.keys(outputRecipes).sort(
+      (a, b) => outputRecipes[a].length - outputRecipes[b].length,
     );
     for (const itemId of order) {
-      for (const option of potentials[itemId]) {
+      for (const option of allRecipes[itemId]) {
         if (!steps.some((s) => s.recipeId === option)) {
           const step = steps.find((s) => s.itemId === itemId);
           if (step) {
