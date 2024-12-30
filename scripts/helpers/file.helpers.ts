4d3ddf6ca589849bca700e0c00dd93f25b6a4fbc
Simon Sapin <simon.sapin@exyr.org>
2024-12-30 22:11:38 +0100

    Only allow some locale files to be missing

--- scripts/helpers/file.helpers.ts
+++ scripts/helpers/file.helpers.ts
@@ -3,17 +3,8 @@ import fs from 'fs';
 import { Locale } from '../factorio-build.models';
 
 export function getJsonData(file: string): unknown {
-  try {
-    const str = fs.readFileSync(file).toString();
-    return JSON.parse(str);
-  } catch (err) {
-    if ((err as any).code === 'ENOENT') {
-      // space-connection-locale.json is not generated when spaceage mod is disabled
-      return {};
-    } else {
-      throw err;
-    }
-  }
+  const str = fs.readFileSync(file).toString();
+  return JSON.parse(str);
 }
 
 const appDataPath =
@@ -26,4 +17,16 @@ export const factorioPath = fs.existsSync(flatpakSteamPath)
 export function getLocale(file: string): Locale {
   const path = `${factorioPath}/script-output/${file}`;
   return getJsonData(path) as Locale;
+}
+
+export function tryGetLocale(file: string): Locale {
+  try {
+    return getLocale(file);
+  } catch (err) {
+    if ((err as any).code === 'ENOENT') {
+      return { names: {} };
+    } else {
+      throw err;
+    }
+  }
 }
