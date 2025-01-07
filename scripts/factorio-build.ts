fbc254c52f18eebff3271eedca55adf866af1778
nathanielemmert <101952729+nathanielemmert@users.noreply.github.com>
2025-01-07 03:18:56 -0500

    removed commented code

--- scripts/factorio-build.ts
+++ scripts/factorio-build.ts
@@ -1318,17 +1318,6 @@ async function processMod(): Promise<void> {
         }
       }
 
-      // Check for calculated catalysts
-      // for (const outId of Object.keys(recipeOut)) {
-      //   if (recipeIn[outId] && !recipeCatalyst?.[outId]) {
-      //     // Need to manually calculate and add catalyst amount for this item
-      //     if (recipeCatalyst == null) recipeCatalyst = {};
-
-      //     const amount = Math.min(recipeOut[outId], recipeIn[outId]);
-      //     recipeCatalyst[outId] = amount;
-      //   }
-      // }
-
       let producers = producersMap.crafting[proto.category ?? 'crafting'];
       if (producers != null) {
         // Ensure producers have sufficient fluid boxes
