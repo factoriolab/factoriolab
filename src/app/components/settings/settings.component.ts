3d0ccc804bdbcfd0193f55263833f9684bb145a9
Doug Broad <dcbroad3@gmail.com>
2025-02-13 15:53:01 -0500

    feat: Add version update handling (#1692)

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -218,9 +218,10 @@ export class SettingsComponent {
           message,
           acceptLabel,
           rejectLabel,
-          accept: () => {
+          accept: async () => {
             localStorage.clear();
-            void this.router.navigate([this.modId()]);
+            await this.router.navigate([this.modId()]);
+            this.contentSvc.reload();
           },
         });
       });
