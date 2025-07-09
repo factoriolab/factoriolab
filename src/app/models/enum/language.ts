72bd70b0bc69f9ea872ab60a5a9dae26cb320d36
Doug Broad <dcbroad3@gmail.com>
2025-07-09 10:26:45 -0400

    Add portuguese support

--- src/app/models/enum/language.ts
+++ src/app/models/enum/language.ts
@@ -7,13 +7,15 @@ export enum Language {
   French = 'fr',
   Russian = 'ru',
   Japanese = 'ja',
+  Portuguese = 'pt-BR',
 }
 
 export const languageOptions: SelectItem<Language>[] = [
   { label: 'English', value: Language.English },
   { label: '简体中文 (Chinese)', value: Language.Chinese },
-  { label: 'Deutsch (German)', value: Language.German },
   { label: 'Français (French)', value: Language.French },
-  { label: 'русский (Russian)', value: Language.Russian },
+  { label: 'Deutsch (German)', value: Language.German },
   { label: '日本語 (Japanese)', value: Language.Japanese },
+  { label: 'Português (Portuguese)', value: Language.Portuguese },
+  { label: 'русский (Russian)', value: Language.Russian },
 ];
