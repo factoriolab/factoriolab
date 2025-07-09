900d5cec946d28893897e5297d9eb96bb7186e7c
Doug Broad <dcbroad3@gmail.com>
2025-07-09 10:56:15 -0400

    fix: Remove hide duplicate icons flag, closes #1726 (#1733)

--- src/app/tests/mocks.ts
+++ src/app/tests/mocks.ts
@@ -222,7 +222,6 @@ export const preferencesState: PreferencesState = {
   theme: Theme.Dark,
   bypassLanding: false,
   showTechLabels: false,
-  hideDuplicateIcons: false,
   paused: false,
   convertObjectiveValues: false,
   flowSettings: flowSettings,
