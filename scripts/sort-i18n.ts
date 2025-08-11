4d270c9c72f72e545cb8f32a93f540164c1982aa
Doug Broad <dcbroad3@gmail.com>
2025-08-11 14:33:26 -0400

    i18n sort tweak

--- scripts/sort-i18n.ts
+++ scripts/sort-i18n.ts
@@ -2,23 +2,20 @@ import fs from 'fs';
 
 import { getJsonData } from './utils/file';
 
-interface SortBatch {
-  original: Record<string, unknown>;
-  clone: Record<string, unknown>;
-}
-
 const i18nPath = 'public/i18n';
 
 fs.readdirSync(i18nPath).forEach((file) => {
   const path = `${i18nPath}/${file}`;
   const data = getJsonData(path) as Record<string, unknown>;
   const result: Record<string, unknown> = {};
-  const stack: SortBatch[] = [{ original: data, clone: result }];
+  const stack: [Record<string, unknown>, Record<string, unknown>][] = [
+    [data, result],
+  ];
 
   while (stack.length) {
     const next = stack.pop();
     if (next == null) break;
-    const { original, clone } = { ...next };
+    const [original, clone] = [...next];
 
     Object.keys(original)
       .sort()
@@ -27,10 +24,7 @@ fs.readdirSync(i18nPath).forEach((file) => {
         if (value && typeof value === 'object') {
           const child = {};
           clone[key] = child;
-          stack.push({
-            original: value as Record<string, unknown>,
-            clone: child,
-          });
+          stack.push([value as Record<string, unknown>, child]);
         } else clone[key] = value;
       });
   }
