bf62ddf1d56864f2a97515988784f106080aad23
Doug Broad <dcbroad3@gmail.com>
2025-11-20 11:43:24 -0500

    Add basic error page

--- src/app/error/error-handler.ts
+++ src/app/error/error-handler.ts
@@ -2,18 +2,22 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { ErrorHandler, inject, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { ErrorInfo } from './error-info';
+
 @Injectable({ providedIn: 'root' })
 export class LabErrorHandler implements ErrorHandler {
   private router = inject(Router);
 
   handleError(error: unknown): void {
     console.error(error);
-    void this.router.navigate(['/error'], { info: this.getDetail(error) });
+    const info: ErrorInfo = { message: this.getMessage(error) };
+    void this.router.navigate(['/error'], { info });
   }
 
-  private getDetail(error: unknown): string | undefined {
+  private getMessage(error: unknown): string | undefined {
     if (typeof error === 'string') return error;
-    if (error instanceof HttpErrorResponse) return error.message;
+    if (error instanceof Error || error instanceof HttpErrorResponse)
+      return error.message;
     return undefined;
   }
 }
--- src/app/error/error.ts
+++ src/app/error/error.ts
@@ -1,23 +1,49 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { CanActivateFn, Navigation, Router } from '@angular/router';
+import { CanActivateFn, Router, RouterLink } from '@angular/router';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import {
+  faExclamationCircle,
+  faHome,
+  faTrash,
+} from '@fortawesome/free-solid-svg-icons';
 
+import { Button } from '~/components/button/button';
 import { DEFAULT_MOD } from '~/data/datasets';
+import { SettingsStore } from '~/state/settings/settings-store';
+import { TranslatePipe } from '~/translate/translate-pipe';
+import { coalesce } from '~/utils/nullish';
 
-function getErrorInfo(navigation: Navigation | null): string | undefined {
-  return navigation?.extras?.info as string | undefined;
-}
+import { getErrorInfo } from './error-info';
 
 export const errorGuard: CanActivateFn = () => {
   const router = inject(Router);
-  if (!getErrorInfo(router.currentNavigation()))
-    return router.createUrlTree([DEFAULT_MOD]);
+  if (!getErrorInfo(router)) return router.createUrlTree([DEFAULT_MOD]);
   return true;
 };
 
 @Component({
   selector: 'lab-error',
-  imports: [],
+  imports: [RouterLink, FaIconComponent, Button, TranslatePipe],
   templateUrl: './error.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'flex w-full justify-center p-4 flex-col items-center h-2/3 gap-4',
+  },
 })
-export class Error {}
+export class Error {
+  private readonly router = inject(Router);
+  protected readonly settingsStore = inject(SettingsStore);
+
+  protected readonly faExclamationCircle = faExclamationCircle;
+  protected readonly faTrash = faTrash;
+  protected readonly faHome = faHome;
+  protected readonly info = getErrorInfo(this.router);
+
+  deleteCustom(): void {
+    this.settingsStore.customData.set(undefined);
+    void this.router.navigate([
+      '/',
+      coalesce(this.settingsStore.modId(), DEFAULT_MOD),
+    ]);
+  }
+}
