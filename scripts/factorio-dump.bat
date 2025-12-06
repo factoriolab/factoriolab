dc6c0a3fe42fcaba42ec4e5920a93c7be9a0805f
Doug Broad <dcbroad3@gmail.com>
2025-12-05 19:09:00 -0500

    Restore dump scripts

---
+++ scripts/factorio-dump.bat
@@ -0,0 +1,5 @@
+cd %ProgramFiles(x86)%\Steam\steamapps\common\Factorio\bin\x64
+
+factorio.exe --dump-data
+factorio.exe --dump-prototype-locale
+"%ProgramFiles(x86)%\Steam\Steam.exe" -applaunch 427520 --dump-icon-sprites
