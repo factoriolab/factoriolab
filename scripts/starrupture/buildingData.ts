beb3846fbf9071edaaed6445a1810095f65d1840
coldino <28345893+coldino@users.noreply.github.com>
2026-05-05 14:35:28 +0100

    feat: Add support for StarRupture (#1788)
    Live site (with SR as the default game): <https://sr-lab.netlify.app>
    
    Strongly recommend squashing during merge!
    
    Current state is complete, but I'm open to improvements
    * Covers all machines and items available at the start of Early Access
    * Has belt options for the Cargo Receiver with calculated throughput
    based on the 10s send delay per stack and the input rail speed (though
    this has to assume the normal 100 stack size)
    * Handles different ore purities (impure/normal/pure) as different
    machines so it's easy to switch them
    
    The only mechanism that isn't covered is "heat" - a per-machine value a
    bit like power draw. I'm not sure it's super necessary as it's also
    location specific (per-core). Certainly not necessary at this stage.
    
    ## Updating the data
    
    Requires:
    * FModel
    * Game mappings (usually acquired from modding sites or directly using
    [ue4ss](<https://github.com/UE4SS-RE/RE-UE4SS>))
    
    Steps:
    1. Acquire game mappings
    2. Setup FModel to point at the game directory and the mapping file, and
    output to `factoriolab/sr-data/`
    3. Load all packages/archives then navigate to
    `StarRupture/Content/Chimera`, right click and:
        * Save Folder's Packages Properties
        * Save Folder's Packages Textures
    4. In `factoriolab` run `npm run starrupture-build`

---
+++ scripts/starrupture/buildingData.ts
@@ -0,0 +1,50 @@
+import { getJsonData } from '../helpers/file.helpers';
+import { normalizeObjectPath } from './utils';
+
+export type BdInfo = {
+  id: string;
+  path: string;
+  iconObjectPath?: string | null;
+  name?: string | null;
+  nameRaw?: { localized?: string; source?: string; key?: string } | null;
+  uiSubType?: string | null;
+  uniqueName?: string | null;
+};
+
+export function parseBdFile(filePath: string): BdInfo {
+  const raw = getJsonData(filePath) as any[];
+  const id = filePath.split(/[\\/]/).pop()?.replace('.json', '') ?? '';
+  const info: BdInfo = {
+    id,
+    path: filePath,
+    iconObjectPath: null,
+    name: null,
+    nameRaw: null,
+    uiSubType: null,
+    uniqueName: null,
+  };
+
+  for (const obj of raw) {
+    if (obj?.Type === 'CrBuildingData') {
+      const props = obj?.Properties ?? {};
+      const icon = props?.Icon?.ResourceObject?.ObjectPath;
+      if (icon) info.iconObjectPath = normalizeObjectPath(icon);
+
+      const bn = props?.BuildingName ?? {};
+      info.nameRaw = {
+        localized: bn?.LocalizedString ?? undefined,
+        source: bn?.SourceString ?? undefined,
+        key: bn?.Key ?? undefined,
+      };
+      info.name = bn?.LocalizedString ?? bn?.SourceString ?? bn?.Key ?? null;
+
+      info.uiSubType = props?.UISubType ?? props?.UIType ?? null;
+      info.uniqueName = props?.BuldingUniqueName ?? props?.BuildingID ?? null;
+
+      // Found primary object, break
+      break;
+    }
+  }
+
+  return info;
+}
