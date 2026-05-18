1ae531dfdece64f444f4d4f85f3dfb34ce3d6d57
Doug Broad <dcbroad3@gmail.com>
2026-05-18 16:57:08 -0400

    50.27% - Objectives tests

--- src/app/main/objectives/objectives.spec.ts
+++ src/app/main/objectives/objectives.spec.ts
@@ -1,4 +1,12 @@
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+
+import {
+  mockObjective1,
+  mockObjective5,
+  mockObjectiveBase,
+} from '~/tests/mocks/objective';
 
 import { Objectives } from './objectives';
 
@@ -18,5 +26,51 @@ describe('Objectives', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('addObjective', () => {
+    it('should call the objectives store with the passed value', () => {
+      spyOn(component['objectivesStore'], 'add');
+      component.addObjective(mockObjectiveBase);
+      expect(component['objectivesStore'].add).toHaveBeenCalledWith(
+        mockObjectiveBase,
+      );
+    });
+  });
+
+  describe('drop', () => {
+    it('should call the objectives store with the new order', () => {
+      spyOn(component['objectivesStore'], 'setOrder');
+      spyOn<any>(component, 'objectives').and.returnValue(['a', 'b']);
+      component.drop({ previousIndex: 0, currentIndex: 1 } as CdkDragDrop<
+        string[]
+      >);
+      expect(component['objectivesStore'].setOrder).toHaveBeenCalledWith([
+        'b',
+        'a',
+      ] as any);
+    });
+  });
+
+  describe('changeTarget', () => {
+    it('should pick a recipe', () => {
+      spyOn(component['picker'], 'pickRecipe').and.returnValue(of('id'));
+      spyOn(component['objectivesStore'], 'updateRecord');
+      component.changeTarget(mockObjective5);
+      expect(component['objectivesStore'].updateRecord).toHaveBeenCalledWith(
+        mockObjective5.id,
+        { targetId: 'id' },
+      );
+    });
+
+    it('should pick an item', () => {
+      spyOn(component['picker'], 'pickItem').and.returnValue(of('id'));
+      spyOn(component['objectivesStore'], 'updateRecord');
+      component.changeTarget(mockObjective1);
+      expect(component['objectivesStore'].updateRecord).toHaveBeenCalledWith(
+        mockObjective1.id,
+        { targetId: 'id' },
+      );
+    });
   });
 });
