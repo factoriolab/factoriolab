5ab156a957f4f30bc071ec27aebb27a68ac3c51a
Doug Broad <dcbroad3@gmail.com>
2026-05-27 20:11:31 -0400

    64.77% - More steps tests

--- src/components/steps/steps.spec.ts
+++ src/components/steps/steps.spec.ts
@@ -2,6 +2,7 @@ import { ApplicationRef } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { initialColumnsState } from '~/state/preferences/columns-state';
+import { ItemId } from '~/tests/item-id';
 import { Mocks } from '~/tests/mocks/mocks';
 import { TestModule } from '~/tests/test-module';
 import { setInputs } from '~/tests/utils';
@@ -62,6 +63,87 @@ describe('Steps', () => {
         },
       });
       expect(component['leftSpan']()).toEqual(4);
+    });
+  });
+
+  describe('toggleStep', () => {
+    it('should toggle a step to be expanded and collapsed', () => {
+      component.toggleStep('id');
+      expect(component['expandedSteps']()).toContain('id');
+      component.toggleStep('id');
+      expect(component['expandedSteps']()).not.toContain('id');
+    });
+  });
+
+  describe('resetStep', () => {
+    it('should call to reset an item and recipe', () => {
+      const step = mocks.step1();
+      spyOn(component['itemsStore'], 'resetId');
+      spyOn(component['recipesStore'], 'resetId');
+      component.resetStep(step);
+      expect(component['itemsStore'].resetId).toHaveBeenCalledWith(
+        step.itemId!,
+      );
+      expect(component['recipesStore'].resetId).toHaveBeenCalledWith(
+        step.recipeId!,
+      );
+    });
+
+    it('should call to reset recipe settings on a recipe objective', () => {
+      let step = mocks.step1();
+      step = { ...step, ...{ recipeObjectiveId: 'id' } };
+      spyOn(component['objectivesStore'], 'updateRecord');
+      component.resetStep(step);
+      expect(component['objectivesStore'].updateRecord).toHaveBeenCalledWith(
+        'id',
+        {
+          machineId: undefined,
+          fuelId: undefined,
+          modules: undefined,
+          beacons: undefined,
+          overclock: undefined,
+        },
+      );
+    });
+  });
+
+  describe('changeModulesBeacons', () => {
+    it('should update recipe modules and beacons', () => {
+      let step = mocks.step1();
+      step = {
+        ...step,
+        ...{ recipeSettings: { machineId: ItemId.AssemblingMachine3 } },
+      };
+      spyOn(component['recipesStore'], 'updateRecord');
+      component.changeModulesBeacons(step, { modules: [], beacons: [] });
+      expect(component['recipesStore'].updateRecord).toHaveBeenCalledWith(
+        step.id,
+        { modules: [], beacons: [] },
+      );
+    });
+
+    it('should update recipe objective modules and beacons', () => {
+      let step = mocks.step1();
+      step = {
+        ...step,
+        ...{
+          recipeObjectiveId: 'id',
+          recipeSettings: { machineId: ItemId.AssemblingMachine3 },
+        },
+      };
+      spyOn(component['objectivesStore'], 'updateRecord');
+      component.changeModulesBeacons(step, { modules: [], beacons: [] });
+      expect(component['objectivesStore'].updateRecord).toHaveBeenCalledWith(
+        'id',
+        { modules: [], beacons: [] },
+      );
+    });
+
+    it('should exit early if no recipe or machineId is found', () => {
+      const step = mocks.step1();
+      spyOn(component['recipesStore'], 'updateRecord');
+      component.changeModulesBeacons(step, { modules: [], beacons: [] });
+      expect(component['recipesStore'].updateRecord).not.toHaveBeenCalled();
     });
   });
 });
