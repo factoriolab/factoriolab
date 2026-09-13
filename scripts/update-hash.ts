c2dd576c695f44f8bb9031615ab5a7f5fafa86e4
Doug Broad <dcbroad3@gmail.com>
2026-09-13 13:42:33 -0400

    fix: Include pipes in hash files, closes #1969 (#1971)

--- scripts/update-hash.ts
+++ scripts/update-hash.ts
@@ -1,34 +1,38 @@
 import fs from 'fs';
 
 import { datasets } from '~/data/datasets';
+import { CUSTOM_MOD } from '~/data/game';
 import { ModData } from '~/data/schema/mod-data';
 import { ModHash } from '~/data/schema/mod-hash';
 import { emptyModHash, updateHash } from '~/utils/hash';
 
 import { getJsonData, writeJsonData } from './utils/file';
+import { logTime } from './utils/log';
 
-const modId = process.argv[2];
+// Load mods from arguments
+let mods = process.argv.slice(2);
+if (mods.length === 0)
+  mods = datasets.mods.filter((m) => m.id !== CUSTOM_MOD).map((m) => m.id);
 
-if (!modId) {
-  throw new Error(
-    'Please specify a mod to process by the folder name, e.g. "1.1" for src/data/1.1',
+logTime(
+  `Starting hash update for ${mods.length.toString()} mod${mods.length > 1 ? 's' : ''}...`,
+);
+
+for (let i = 0; i < mods.length; i++) {
+  const mod = mods[i];
+  const modPath = `./public/data/${mod}`;
+  const modDataPath = `${modPath}/data.json`;
+  const modHashPath = `${modPath}/hash.json`;
+
+  const modData = getJsonData(modDataPath) as ModData;
+  const modHash = fs.existsSync(modHashPath)
+    ? (getJsonData(modHashPath) as ModHash)
+    : emptyModHash();
+
+  updateHash(modData, modHash);
+  writeJsonData(modHashPath, modHash);
+
+  logTime(
+    `Updated mod '${mod}' (${(i + 1).toString()} of ${mods.length.toString()})`,
   );
 }
-
-const mod = datasets.mods.find((m) => m.id === modId);
-if (!mod)
-  throw new Error(
-    'Please define this mod set in `src/data/index.ts` before running build.',
-  );
-
-const modPath = `./public/data/${modId}`;
-const modDataPath = `${modPath}/data.json`;
-const modHashPath = `${modPath}/hash.json`;
-
-const modData = getJsonData(modDataPath) as ModData;
-const modHash = fs.existsSync(modHashPath)
-  ? (getJsonData(modHashPath) as ModHash)
-  : emptyModHash();
-
-updateHash(modData, modHash);
-writeJsonData(modHashPath, modHash);
